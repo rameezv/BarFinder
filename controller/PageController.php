18ea001dfdc43eaed6d5b61b62fedda2b27005c4
Rameez <rameez@bigtreeworld.com>
2016-03-24 15:35:06 -0600

    Renamed Website to BarMeUp

--- controller/PageController.php
+++ controller/PageController.php
@@ -18,13 +18,9 @@ class PageController {
 
     public function displayHome() {
         include 'view/header.php';
-
         include 'view/map.php';
-
         include 'view/menu.php';
-
         include 'view/info.php';
-
         include 'view/footer.php';
     }
 
