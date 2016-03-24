e979175df628c81ee5d5fe695d4266dc9fb1b508
Rameez <rameez@bigtreeworld.com>
2016-03-24 15:36:26 -0600

    Propagated rename to the footer as well

--- view/footer.php
+++ view/footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-&copy; Copyright 2016 BarFinder. Created by Rameez Virji and Jatinkumar Patel. All Rights Reserved.
+&copy; Copyright 2016 BarMeUp. Created by Rameez Virji and Jatinkumar Patel. All Rights Reserved.
 </div>
 </body>
 </html>
