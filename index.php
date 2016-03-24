e71ebfc9acd6236212af9cf2a617d6ea56d3a439
Rameez <rameez@bigtreeworld.com>
2016-03-24 10:52:44 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+include 'view/header.php';
+
+?>
+
+<h1>BarFinder</h1>
---
+++ view/header.php
@@ -0,0 +1,6 @@
+<!doctype html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.2/angular.min.js"></script>
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+</head>
